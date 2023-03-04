e0dc3441e8e0900ca157de915c26d3f7ab1df470
Yuliya Safronova <safronova.yuv@gmail.com>
2023-03-04 17:35:46 +0400

    feat: add popup to edit user name and additional info, including mobile layout

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+const editProfileButton = document.querySelector('.button_type_edit');
+const editProfilePopup = document.querySelector('.popup_type_profile-edit');
+const closeProfileButton = document.querySelector('.popup__toggle');
+
+const userNameElement = document.querySelector('.profile__name');
+let userName = userNameElement.textContent;
+const userAboutElement = document.querySelector('.profile__additional');
+let userAbout = userAboutElement.textContent;
+
+const userNameInput = document.querySelector('.popup__input_type_name');
+userNameInput.value = userName;
+
+const userAboutInput = document.querySelector('.popup__input_type_additional');
+userAboutInput.value = userAbout;
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+editProfileButton.addEventListener('click', function() {
+  openPopup(editProfilePopup);
+});
+
+closeProfileButton.addEventListener('click', function() {
+  closePopup(editProfilePopup);
+});
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  let newUserName = userNameInput.value;
+  let newUserAbout = userAboutInput.value;
+  userNameElement.textContent = newUserName;
+  userAboutElement.textContent = newUserAbout;
+  closePopup(editProfilePopup);
+}
+
+const formElement = document.querySelector('.popup__container[name="edit-form"]');
+formElement.addEventListener('submit', handleFormSubmit);

8f628b409d47480a37a20e4e3e4f03ef4da2420e
Yuliya S <safronova.yuv@gmail.com>
2023-04-15 13:38:08 +0400

    feat: close popup by ESC

--- scripts/index.js
+++ scripts/index.js
@@ -24,10 +24,19 @@ inputUserAbout.value = userAbout;
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', handleEscapeClosePopup);
+}
+
+function handleEscapeClosePopup(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', handleEscapeClosePopup);
 }
 
 buttonOpenPopupProfile.addEventListener('click', function() {
